da06c7b2a12e7804707052c43cfef076432c1b62
iilqz15 <z.hayyan21@gmail.com>
2025-12-05 10:16:48 +0300

    ASTParser

--- src/ast_parser.py
+++ src/ast_parser.py
@@ -1,1 +1,15 @@
+import ast
 
+class ASTParser:
+    def __init__(self,code):
+        self.code = code
+        self.tree = None # placeholder for the AST
+
+    def parse(self):
+        self.tree = ast.parse(self.code)
+        return self.tree
+
+    def get_tree(self):
+        if self.tree is None:
+            self.parse()
+        return self.tree
